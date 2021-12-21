d3e0b4a11dd590ee11ac3e5f973b092cca79c04c
John Torres <apodpm@gmail.com>
2021-12-22 01:56:15 +0800

    use: getopt

--- README.md
+++ README.md
@@ -1,1 +1,78 @@
-# eth-lottary
+# Lottery by Ethereum Blockchain
+
+## Set your web3 provider url in `.env`
+
+```text
+PROVIDER=https://mainnet.infura.io/v3/<YOUR-INFURA-TOKEN>
+```
+
+## Create your source file `./data/input.txt`
+
+For example:
+```txt
+lorem
+ipsum
+dolor
+sit
+amet
+consectetur
+adipiscing
+elit
+curabitur
+curabitur
+curabitur
+curabitur
+ornare
+commodo
+eros
+sed
+cursus
+```
+
+## Run
+
+```bash
+$ lottery.py -n <block-number> -p <pick-out>
+```
+
+For example:
+```bash
+$ lottery.py -n 12345678 -p 5
+#
+# Read `input.txt`:
+#  ... lorem
+#  ... ipsum
+#  ... dolor
+#  ... sit
+#  ... amet
+#  ... consectetur
+#  ... adipiscing
+#  ... elit
+#  ... curabitur
+#  ... ornare
+#  ... commodo
+#  ... eros
+#  ... sed
+#  ... cursus
+#
+# --- Pick 5 from 14 ---
+#
+# Write `output.txt`:
+#  #12345678 - 0xb2a8b39935a5eb4b7c9b0117bca06c8d2c0629e0937d20e62c44aace6f05bda3 % 5 = 9 :: ornare
+#  #12345679 - 0xac7f164008dd1bdf29c6c8799cb737d41663fa7d3215b38dfee20135c87a12e4 % 5 = 4 :: amet
+#  #12345680 - 0x189bad2344f34ac6332e522a2c3aceaa28b4eddee63f842109c67cb16360fc3f % 5 = 13 :: cursus
+#  #12345681 - 0x7c9d621ef426c60824a7d5e9871cde3376bcb48f17d1c209d6a6f033f493a090 % 5 = 4
+#  #12345682 - 0xb63493b5909cb9aebb7d7bc0078369aee6a8b4c4b6f928d15e6f101f8a8148e3 % 5 = 3 :: sit
+```
+
+## Then
+
+You will get `./data/output.txt` and `./data/log.txt`
+
+`./data/output.txt`:
+```text
+ornare
+amet
+cursus
+sit
+```
--- lottery.py
+++ lottery.py
@@ -1,16 +1,22 @@
 import os
+import sys
+import getopt
 from lib import settings
 from lib import functions as fn
 
 web3 = settings.PROVIDER
 
-START_BLOCK = 12345678
-
-OUTPUT_COUNT = 13
-OUTPUT_COUNT_LENGTH = 2
+START_BLOCK = 0
+PICK_OUT = 1
+PICK_OUT_LENGTH = 1
 
 output = []
 lines = []
+
+
+def exit_with_help():
+    print('\nlottery.py -n <block-number> -p <pick-out>')
+    sys.exit()
 
 
 def touch(block_number):
@@ -18,38 +24,66 @@ def touch(block_number):
     block_hash = block.hash.hex()
     block_hash_decimal = int(block_hash, 16)
     index = block_hash_decimal % len(rows)
-    index_str = str(index).rjust(OUTPUT_COUNT_LENGTH, ' ')
+    index_str = str(index).rjust(PICK_OUT_LENGTH, ' ')
 
     if rows[index] not in output:
         output.append(rows[index])
-        line = ' #{} - {} % {} = {} :: {}'.format(block_number, block_hash, OUTPUT_COUNT, index_str, rows[index])
+        line = ' #{} - {} % {} = {} :: {}'.format(block_number, block_hash, PICK_OUT, index_str, rows[index])
         lines.append(line)
         print(line)
     else:
-        line = ' #{} - {} % {} = {}'.format(block_number, block_hash, OUTPUT_COUNT, index_str)
+        line = ' #{} - {} % {} = {}'.format(block_number, block_hash, PICK_OUT, index_str)
         lines.append(line)
         print(line)
 
 
+def log(s):
+    lines.append(s)
+    print(s)
+
+
 if __name__ == '__main__':
-    print('\nRead `input.txt`:')
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'hn:p:', ['block-number=', 'pick-out='])
+        for opt, arg in opts:
+            if opt in ('-h', '--help'):
+                exit_with_help()
+            elif opt in ('-n', '--block-number'):
+                try:
+                    START_BLOCK = int(arg)
+                except:
+                    exit_with_help()
+            elif opt in ('-p', '--pick-out'):
+                try:
+                    PICK_OUT = int(arg)
+                except:
+                    exit_with_help()
+    except getopt.GetoptError:
+        exit_with_help()
+
+    if START_BLOCK == 0:
+        exit_with_help()
+
+    PICK_OUT_LENGTH = len(str(PICK_OUT))
+
+    # start here
+    print('\n')
+    log('Read `input.txt`:')
 
     rows = fn.read_list_from_file(os.path.join(settings.DATA_DIR, 'input.txt'))
     rows = fn.remove_duplicated_from_list(rows)
     for row in rows:
-        print(' ... {}'.format(row))
+        log(' ... {}'.format(row))
     length = len(rows)
 
-    if OUTPUT_COUNT > length:
-        raise Exception('`OUTPUT_COUNT = {}`, but `len(rows) = {}`'.format(OUTPUT_COUNT, length))
+    if PICK_OUT > length:
+        raise Exception('`OUTPUT_COUNT = {}`, but `len(rows) = {}`'.format(PICK_OUT, length))
 
-    line1 = '--- Pick {} from {} ---'.format(OUTPUT_COUNT, length)
-    lines.append(line1)
-    print('\n{}'.format(line1))
+    log('\n--- Pick {} from {} ---'.format(PICK_OUT, length))
+    log('\nWrite `output.txt`:')
 
-    print('\nWrite `output.txt`:')
     i = 0
-    while i < OUTPUT_COUNT:
+    while i < PICK_OUT:
         nonce = START_BLOCK + i
         touch(nonce)
         i += 1
